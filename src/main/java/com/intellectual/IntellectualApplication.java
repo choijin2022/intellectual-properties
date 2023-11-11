9e32af1b23d5cd40cfd63af7d2c48770c9138f6c
choeji2022 <choeji2022@gmail.com>
2023-11-11 21:24:21 +0900

    프로젝트 생성

---
+++ src/main/java/com/intellectual/IntellectualApplication.java
@@ -0,0 +1,13 @@
+package com.intellectual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellectualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellectualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intellectual/IntellectualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intellectual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellectualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
